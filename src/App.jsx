3ca9ba8c64f7e069852957016a42f18689a90ea7
AgustinGD <gonzalezdiazagustine@gmail.com>
2022-05-02 21:33:22 -0300

    Router hacia todas las page

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,22 @@ import ProductList from "./pages/ProductList";
 import Register from "./pages/Register";
 import Login from "./pages/Login";
 import Cart from "./pages/Cart";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 const App = () => {
-  return <Home/>;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/product" element={<Product />} />
+        <Route path="/productlist" element={<ProductList />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </Router>
+  );
+  //return <Home/>;
 };
 
 export default App;
