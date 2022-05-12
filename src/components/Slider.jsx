08b7a1c90bed4ace4fa01b327f43d5480751fe00
AgustinGD <gonzalezdiazagustine@gmail.com>
2022-05-12 19:38:50 -0300

    Opcion responsive para el Slider

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -2,7 +2,7 @@ import { ArrowLeftOutlined, ArrowRightOutlined } from "@material-ui/icons";
 import { useState } from "react";
 import styled from "styled-components";
 import { sliderItems } from "../data";
-import { mobile } from "../responsive";
+import { mobile, tablet } from "../responsive";
 import { Link } from "react-router-dom";
 
 const Container = styled.div`
@@ -51,15 +51,17 @@ const Slide = styled.div`
 const ImgContainer = styled.div`
   height: 100%;
   flex: 1;
+  ${tablet({ display: "none" })}
 `;
 
 const Image = styled.img`
-  height: 80%;
+  height: 80%;  
 `;
 
 const InfoContainer = styled.div`
   flex: 1;
   padding: 50px;
+  ${tablet({ textAlign: "center" })}
 `;
 
 const Title = styled.h1`
