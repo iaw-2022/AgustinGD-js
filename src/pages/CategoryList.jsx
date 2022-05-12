8eb4cf87db72fd140d99f0590731df531892bd47
AgustinGD <gonzalezdiazagustine@gmail.com>
2022-05-12 17:06:04 -0300

    Acomodando la UI de Categorias

--- src/pages/CategoryList.jsx
+++ src/pages/CategoryList.jsx
@@ -18,6 +18,11 @@ const Top = styled.div`
   padding: 20px;
 `;
 
+const Title = styled.h1`
+  margin: 20px;
+  text-align: center;
+`;
+
 const TopButton = styled.button`
   padding: 10px;
   font-weight: 600;
@@ -28,8 +33,24 @@ const TopButton = styled.button`
   color: ${(props) => props.type === "filled" && "white"};
 `;
 
-const Header = styled.h1`
-  text-align: center;
+const FilterContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
+
+const Filter = styled.div`
+  display: flex;
+  flex-direction: row;
+  margin: 20px;
+  align-items: center;
+  ${mobile({ width: "0px 20px", display: "flex", flexDirection: "column" })}
+`;
+
+const FilterText = styled.span`
+  font-size: 20px;
+  font-weight: 600;
+  margin-right: 20px;
+  ${mobile({ marginRight: "0px" })}
 `;
 
 const ProductList = (props) => {
@@ -40,13 +61,18 @@ const ProductList = (props) => {
     <Container>
       <Navbar productosEnCarrito={productosEnCarrito}/>
       <Announcement />
-      <Top>
-        <SearchBar setTerminoBusqueda={setTerminoBusqueda} />
-        <Header>Comprar por Categoria</Header>
+      <Top>        
+        <Title>Comprar por Categoria</Title>
         <Link to="/">        
           <TopButton >CONTINUE SHOPPING</TopButton>
         </Link>
       </Top>
+      <FilterContainer>
+        <Filter>
+          <FilterText>Filtrar Categorias:</FilterText>
+          <SearchBar setTerminoBusqueda={setTerminoBusqueda} />
+        </Filter>       
+      </FilterContainer>
       <Categories terminoBusqueda={terminoBusqueda} />
       <Newsletter />
       <Footer />
