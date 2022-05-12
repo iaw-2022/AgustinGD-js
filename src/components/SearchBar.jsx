d40049da225c0cbcf6d4d42e5ff3037895a19ba2
AgustinGD <gonzalezdiazagustine@gmail.com>
2022-05-12 14:43:37 -0300

    Barra de busquedas como componente

---
+++ src/components/SearchBar.jsx
@@ -0,0 +1,44 @@
+import { Search } from "@material-ui/icons";
+import React from "react";
+import styled from "styled-components";
+import { mobile } from "../responsive";
+
+const Container = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    padding: 20px;
+`;
+
+const Language = styled.span`
+font-size: 14px;
+cursor: pointer;
+${mobile({ display: "none" })}
+`;
+
+const SearchContainer = styled.div`
+border: 0.5px solid lightgray;
+display: flex;
+align-items: center;
+margin-left: 25px;
+padding: 5px;
+`;
+
+const Input = styled.input`
+border: none;
+${mobile({ width: "50px" })}
+`;
+
+const SearchBar = () => {    
+    return (
+        <Container>
+            <Language>ES </Language>
+            <SearchContainer>
+            <Input placeholder="Buscar" />
+            <Search style={{ color: "gray", fontSize: 16 }} />
+            </SearchContainer>
+        </Container>
+    );
+};
+
+export default SearchBar;
