20d79b13842b72625527c5bbdd085d1a132ef534
AgustinGD <gonzalezdiazagustine@gmail.com>
2022-05-13 14:54:07 -0300

    Actualizando contacto (no real)

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -128,7 +128,7 @@ const Footer = () => {
           <Phone style={{marginRight:"10px"}}/> +1 234 56 78
         </ContactItem>
         <ContactItem>
-          <MailOutline style={{marginRight:"10px"}} /> contact@lama.dev
+          <MailOutline style={{marginRight:"10px"}} /> contacto@mei.granja
         </ContactItem>
         <Payment src="https://i.ibb.co/Qfvn4z6/payment.png" />
       </Right>
