5fa03adcda549cc1a8f09cb474db9f221228f827
AgustinGD <gonzalezdiazagustine@gmail.com>
2022-05-14 17:11:22 -0300

    Nombre de usuario te lleva a pedidos

--- src/components/OpcionesUsuario.jsx
+++ src/components/OpcionesUsuario.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import styled from "styled-components";
 import { mobile } from "../responsive";
 import { useAuth0 } from "@auth0/auth0-react";
+import { Link } from "react-router-dom";
+import { ORDER_LIST_PATH } from '../utils/Constants';
 
 const MenuItem = styled.div`
   font-size: 14px;
@@ -15,10 +17,12 @@ const Perfil = () => {
     const { user, isAuthenticated } = useAuth0();
 
     return (
-        isAuthenticated && ( 
-        <MenuItem>
-            {user.name}
-        </MenuItem>
+        isAuthenticated && (
+        <Link to={ORDER_LIST_PATH} style={{ textDecoration: "none", color: "black"}}>
+            <MenuItem>
+                {user.name}
+            </MenuItem>
+        </Link> 
         )
     )
 };
