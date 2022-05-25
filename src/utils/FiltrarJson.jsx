4957b99ee80f27eb02430a52b757fae6b35e2487
AgustinGD <gonzalezdiazagustine@gmail.com>
2022-05-25 19:34:40 -0300

    fix al filtro

--- src/utils/FiltrarJson.jsx
+++ src/utils/FiltrarJson.jsx
@@ -1,13 +1,9 @@
 
-export const filtrarNombre = (items, filtro) =>{
-    const resultado = items.filter(matchName(items, filtro))
+export const filtrarNombre = (items, filtro) =>{    
+    const resultado = items.filter( item => matchName(item, filtro))
     return resultado;
 }
 
 const matchName = (item, filtro) => {
-    if(filtro === ""){
-        return item
-    } else if (item.nombre.toLowerCase().includes(filtro.toLowerCase())) {
-        return item
-    }
+    return (filtro === "") || (item.nombre.toLowerCase().includes(filtro.toLowerCase()))
 }
