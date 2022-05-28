f9879039c8d8a232e80c4e4c707b02711cfca207
AgustinGD <gonzalezdiazagustine@gmail.com>
2022-05-28 17:28:22 -0300

    path para page NotFound

--- src/utils/Constants.jsx
+++ src/utils/Constants.jsx
@@ -1,6 +1,7 @@
 export const HOME_PATH = "/";
 export const SHOPPING_CART_PATH = "/Carrito-De-Compras";
 export const PRODUCT_PATH = "/Producto";
-export const PRODUCT_LIST_PATH = "/Productos/Categoria";
+export const PRODUCT_LIST_PATH = "/Categoria";
 export const CATEGORY_LIST_PATH = "/Nuestras-Categorias";
 export const ORDER_LIST_PATH = "/Mis-Pedidos";
+export const NOT_FOUND_PATH = "*"
