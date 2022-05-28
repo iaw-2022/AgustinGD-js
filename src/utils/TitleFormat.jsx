2f19d7f54044d4c902a14d5f608e39601746d434
AgustinGD <gonzalezdiazagustine@gmail.com>
2022-05-28 08:09:32 -0300

    indentacion

--- src/utils/TitleFormat.jsx
+++ src/utils/TitleFormat.jsx
@@ -1,8 +1,8 @@
-export const titleFormat = (title) =>{
+export const titleFormat = (title) => {
     const result = title.toString().toLowerCase()
     return capitalize(result)
 }
 
 const capitalize = (title) => {
-    return title.charAt(0).toUpperCase() + title.slice(1); 
+    return title.charAt(0).toUpperCase() + title.slice(1);
 }
